4fb78acf7c5d680f509d29edce4f37b3732fe5f6
Romik110 <117544663+Romik110@users.noreply.github.com>
2023-08-18 00:28:51 +0300

    Update CM_OBJECT_SEARCH.java
    +add  Teleport GM to Npc (Through search)

--- src/com/aionemu/gameserver/network/aion/clientpackets/CM_OBJECT_SEARCH.java
+++ src/com/aionemu/gameserver/network/aion/clientpackets/CM_OBJECT_SEARCH.java
@@ -17,10 +17,12 @@
 package com.aionemu.gameserver.network.aion.clientpackets;
 
 import com.aionemu.gameserver.dataholders.DataManager;
+import com.aionemu.gameserver.model.gameobjects.player.Player;
 import com.aionemu.gameserver.model.templates.spawns.SpawnSearchResult;
 import com.aionemu.gameserver.network.aion.AionClientPacket;
 import com.aionemu.gameserver.network.aion.AionConnection.State;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_SHOW_NPC_ON_MAP;
+import com.aionemu.gameserver.services.teleport.TeleportService2;
 
 /**
  * @author Lyahim
@@ -34,27 +36,36 @@ public class CM_OBJECT_SEARCH extends AionClientPacket {
 	 * 
 	 * @param opcode
 	 */
-	public CM_OBJECT_SEARCH(int opcode, State state, State... restStates) {
-		super(opcode, state, restStates);
-
-	}
+	public CM_OBJECT_SEARCH(int opcode, State state, State... restStates)
+		{
+			super(opcode, state, restStates);
+		}
 
 	/**
 	 * Nothing to do
 	 */
 	@Override
-	protected void readImpl() {
-		this.npcId = readD();
-	}
+	protected void readImpl()
+		{
+			this.npcId = readD();
+		}
 
 	/**
 	 * Logging
 	 */
 	@Override
-	protected void runImpl() {
+	protected void runImpl() 
+	{
 		SpawnSearchResult searchResult = DataManager.SPAWNS_DATA2.getFirstSpawnByNpcId(0, npcId);
-		if (searchResult != null) {
+    	Player player = getConnection().getActivePlayer();
+		if (searchResult != null)
+
+		{
 			sendPacket(new SM_SHOW_NPC_ON_MAP(npcId, searchResult.getWorldId(), searchResult.getSpot().getX(), searchResult.getSpot().getY(), searchResult.getSpot().getZ()));
+      		if (player.isGM()) 
+			{
+        		TeleportService2.teleportToNpc(player, this.npcId);
+			}
 		}
 	}
 }
