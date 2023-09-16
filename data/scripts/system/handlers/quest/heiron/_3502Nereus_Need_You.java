7a1fbdbfa3a9a5cc1deb1668cf37d015657131a0
Dezalmado <51215558+dezalmado@users.noreply.github.com>
2023-09-16 08:30:35 -0300

    Thank you very much @kortana 3502 Nereus Need You

--- data/scripts/system/handlers/quest/heiron/_3502Nereus_Need_You.java
+++ data/scripts/system/handlers/quest/heiron/_3502Nereus_Need_You.java
@@ -14,9 +14,8 @@
  *  You should have received a copy of the GNU General Public License
  *  along with aion-lightning.  If not, see <http://www.gnu.org/licenses/>.
  */
-package quest.heiron;
+package quest.dark_poeta;
 
-import com.aionemu.gameserver.model.gameobjects.Npc;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
 import com.aionemu.gameserver.questEngine.handlers.QuestHandler;
 import com.aionemu.gameserver.questEngine.model.QuestDialog;
@@ -25,14 +24,12 @@ import com.aionemu.gameserver.questEngine.model.QuestState;
 import com.aionemu.gameserver.questEngine.model.QuestStatus;
 
 /****/
-/** Author Ghostfur & Unknown (Aion-Unique)
+/** Author Kortana (Encom)
 /****/
 
 public class _3502Nereus_Need_You extends QuestHandler
 {
 	private final static int questId = 3502;
-	
-	private final static int[] npcs = {204656, 730192};
 	private final static int[] mobs = {214894, 214895, 214896, 214897, 214904};
 	
 	public _3502Nereus_Need_You() {
@@ -40,45 +37,54 @@ public class _3502Nereus_Need_You extends QuestHandler
 	}
 	
 	@Override
-	public boolean onLvlUpEvent(QuestEnv env) {
-		return defaultOnLvlUpEvent(env);
-	}
-	
-	@Override
 	public void register() {
-		qe.registerOnLevelUp(questId);
 		qe.registerQuestNpc(204656).addOnQuestStart(questId);
-		for (int npc: npcs) {
-			qe.registerQuestNpc(npc).addOnTalkEvent(questId);
-		} for (int mob: mobs) {
+		qe.registerQuestNpc(204656).addOnTalkEvent(questId);
+		qe.registerQuestNpc(730192).addOnTalkEvent(questId);
+		for (int mob: mobs) {
 			qe.registerQuestNpc(mob).addOnKillEvent(questId);
 		}
 	}
 	
 	@Override
-	public boolean onDialogEvent(QuestEnv env) {
+	public boolean onDialogEvent(final QuestEnv env) {
 		final Player player = env.getPlayer();
         final QuestState qs = player.getQuestStateList().getQuestState(questId);
 		int targetId = env.getTargetId();
-		int var = qs.getQuestVarById(0);
-		QuestDialog dialog = env.getDialog();
-		if (env.getVisibleObject() instanceof Npc) {
-            targetId = ((Npc) env.getVisibleObject()).getNpcId();
-        } if (qs.getStatus() == QuestStatus.START) {
-			switch (targetId) {
-				case 730192: { //Balaur Operation Orders.
-					if (dialog == QuestDialog.USE_OBJECT && var == 0) {
-						return sendQuestDialog(env, 1011);
-					} if (dialog == QuestDialog.STEP_TO_1) {
-						return defaultCloseDialog(env, 0, 1);
+		if (qs == null || qs.getStatus() == QuestStatus.NONE) {
+			if (targetId == 204656) {
+                switch (env.getDialog()) {
+                    case START_DIALOG: {
+                        return sendQuestDialog(env, 4762);
+					} case ASK_ACCEPTION: {
+					    return sendQuestDialog(env, 4);
+					} case ACCEPT_QUEST: {
+						return sendQuestStartDialog(env);
+					} case REFUSE_QUEST: {
+				        return closeDialogWindow(env);
 					}
-					break;
+                }
+			}
+		} else if (qs != null && qs.getStatus() == QuestStatus.START) {
+			int var = qs.getQuestVarById(0);
+			if (targetId == 730192) {
+				switch (env.getDialog()) {
+					case START_DIALOG: {
+						if (var == 0) {
+							return sendQuestDialog(env, 1011);
+						}
+					} case STEP_TO_1: {
+						changeQuestStep(env, 0, 1, false);
+						return closeDialogWindow(env);
+					}
 				}
 			}
-		} else if (qs.getStatus() == QuestStatus.REWARD) {
-			if (targetId == 204656) { //Maloren.
-				if (dialog == QuestDialog.START_DIALOG) {
-					return sendQuestDialog(env, 10002);
+		} else if (qs != null && qs.getStatus() == QuestStatus.REWARD) {
+            if (targetId == 204656) {
+                if (env.getDialog() == QuestDialog.USE_OBJECT) {
+                    return sendQuestDialog(env, 10002);
+				} else if (env.getDialog() == QuestDialog.SELECT_REWARD) {
+					return sendQuestDialog(env, 5);
 				} else {
 					return sendQuestEndDialog(env);
 				}
@@ -89,22 +95,21 @@ public class _3502Nereus_Need_You extends QuestHandler
 	
 	@Override
 	public boolean onKillEvent(QuestEnv env) {
-		Player player = env.getPlayer();
-		QuestState qs = player.getQuestStateList().getQuestState(questId);
-		Npc npc = (Npc) env.getVisibleObject();
-		int targetId = npc.getNpcId();
+		final Player player = env.getPlayer();
+        final QuestState qs = player.getQuestStateList().getQuestState(questId);
+		int targetId = env.getTargetId();
 		if (qs != null && qs.getStatus() == QuestStatus.START) {
 			int var = qs.getQuestVarById(0);
 			int var1 = qs.getQuestVarById(1);
 			int var2 = qs.getQuestVarById(2);
 			int var3 = qs.getQuestVarById(3);
 			switch (targetId) {
-				case 214894: //Telepathy Controller.
+				case 214894:
 					if (var == 1) {
 						return defaultOnKillEvent(env, 214894, 1, 2, 0);
 					}
 				break;
-				case 214895: //Main Power Generator.
+				case 214895:
 					if (var == 2 && var1 != 1) {
 						defaultOnKillEvent(env, 214895, 0, 1, 1);
 						if (var2 == 1 && var3 == 1) {
@@ -113,7 +118,7 @@ public class _3502Nereus_Need_You extends QuestHandler
 						return true;
 					}
 				break;
-				case 214896: //Auxiliary Power Generator.
+				case 214896:
 					if (var == 2 && var2 != 1) {
 						defaultOnKillEvent(env, 214896, 0, 1, 2);
 						if (var1 == 1 && var3 == 1) {
@@ -122,7 +127,7 @@ public class _3502Nereus_Need_You extends QuestHandler
 						return true;
 					}
 				break;
-				case 214897: //Emergency Generator.
+				case 214897:
 					if (var == 2 && var3 != 1) {
 						defaultOnKillEvent(env, 214897, 0, 1, 3);
 						if (var1 == 1 && var2 == 1) {
@@ -131,7 +136,7 @@ public class _3502Nereus_Need_You extends QuestHandler
 						return true;
 					}
 				break;
-				case 214904: //Brigade General Anuhart.
+				case 214904:
 					if (var == 2 && var1 == 1 && var2 == 1 && var3 == 1) {
 						return defaultOnKillEvent(env, 214904, 2, true);
 					}
