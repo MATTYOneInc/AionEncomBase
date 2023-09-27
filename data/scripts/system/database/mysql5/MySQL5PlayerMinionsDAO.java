bf39036dce7aaab2401b9efe71c6213ab713811f
pressure68 <pressure68@users.noreply.github.com>
2023-09-27 09:05:40 -0400

    clean up comments
    I removed certain offensive or inappropriate language in code comments that has been brought up a few times on the forums.

--- data/scripts/system/database/mysql5/MySQL5PlayerMinionsDAO.java
+++ data/scripts/system/database/mysql5/MySQL5PlayerMinionsDAO.java
@@ -1,6 +1,4 @@
 /*
- * This file is part of Encom. **ENCOM FUCK OTHER SVN**
- *
  *  Encom is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
--- src/com/aionemu/gameserver/services/player/CreativityPanel/stats/Accuracy.java
+++ src/com/aionemu/gameserver/services/player/CreativityPanel/stats/Accuracy.java
@@ -1,5 +1,5 @@
 /*
- * This file is part of Encom. **ENCOM FUCK OTHER SVN**
+
  *
  *  Encom is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser Public License as published by
