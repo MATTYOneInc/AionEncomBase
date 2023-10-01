cef2e146d33c7b9031f421a846bd7df2e1f282f9
Dezalmado <51215558+dezalmado@users.noreply.github.com>
2023-10-01 15:08:38 -0300

    ID: 18970 Name: The Corridor Lore "fixed"

--- data/scripts/system/handlers/quest/cygnea/_18970The_Corridor_Lore.java
+++ data/scripts/system/handlers/quest/cygnea/_18970The_Corridor_Lore.java
@@ -1,14 +1,18 @@
-/*
- * =====================================================================================*
- * This file is part of Aion-Unique (Aion-Unique Home Software Development)             *
- * Aion-Unique Development is a closed Aion Project that use Old Aion Project Base      *
- * Like Aion-Lightning, Aion-Engine, Aion-Core, Aion-Extreme, Aion-NextGen, ArchSoft,   *
- * Aion-Ger, U3J, Encom And other Aion project, All Credit Content                      *
- * That they make is belong to them/Copyright is belong to them. And All new Content    *
- * that Aion-Unique make the copyright is belong to Aion-Unique                         *
- * You may have agreement with Aion-Unique Development, before use this Engine/Source   *
- * You have agree with all of Term of Services agreement with Aion-Unique Development   *
- * =====================================================================================*
+/**
+ * This file is part of Aion-Lightning <aion-lightning.org>.
+ *
+ *  Aion-Lightning is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Aion-Lightning is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details. *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Aion-Lightning.
+ *  If not, see <http://www.gnu.org/licenses/>.
  */
 package quest.cygnea;
 
@@ -22,85 +26,55 @@ import com.aionemu.gameserver.questEngine.model.QuestState;
 import com.aionemu.gameserver.questEngine.model.QuestStatus;
 import com.aionemu.gameserver.utils.PacketSendUtility;
 
-/****/
-/** Author Ghostfur & Unknown (Aion-Unique)
-/****/
+/**
+ * @author pralinka
+ * @rework ddmbest88
+ */
+public class _18970The_Corridor_Lore extends QuestHandler {
 
-public class _18970The_Corridor_Lore extends QuestHandler
-{
-	private final static int questId = 18970;
-	
+	public static final int questId = 18970;
+
 	public _18970The_Corridor_Lore() {
 		super(questId);
 	}
-	
+
 	@Override
 	public void register() {
 		qe.registerQuestNpc(804709).addOnQuestStart(questId);
-		qe.registerQuestNpc(804709).addOnTalkEvent(questId);
 		qe.registerQuestNpc(804865).addOnTalkEvent(questId);
-		qe.registerQuestNpc(805213).addOnTalkEvent(questId);
-		qe.registerQuestNpc(805214).addOnTalkEvent(questId);
 		qe.registerQuestNpc(805215).addOnTalkEvent(questId);
 	}
-	
+
 	@Override
 	public boolean onDialogEvent(QuestEnv env) {
-		final Player player = env.getPlayer();
+		Player player = env.getPlayer();
+		int targetId = env.getTargetId();
 		QuestState qs = player.getQuestStateList().getQuestState(questId);
-		int targetId = 0;
-		if (env.getVisibleObject() instanceof Npc)
-			targetId = ((Npc) env.getVisibleObject()).getNpcId();
+
 		if (qs == null || qs.getStatus() == QuestStatus.NONE) {
 			if (targetId == 804709) {
 				if (env.getDialog() == QuestDialog.START_DIALOG) {
 					return sendQuestDialog(env, 4762);
-				} else {
+				}
+				else {
 					return sendQuestStartDialog(env);
 				}
 			}
-		} if (qs == null)
-			return false;
-		if (qs.getStatus() == QuestStatus.START) {
-			switch (targetId) {
-				case 804865: {
-					switch (env.getDialog()) {
-						case START_DIALOG: {
-							return sendQuestDialog(env, 1352);
-						} case STEP_TO_1: {
-							qs.setQuestVarById(0, qs.getQuestVarById(0) + 1);
-							updateQuestStatus(env);
-							PacketSendUtility.sendPacket(player, new SM_DIALOG_WINDOW(env.getVisibleObject().getObjectId(), 10));
-							return true;
-						}
-					}
+		}
+		else if (qs.getStatus() == QuestStatus.START) {
+			if ((targetId == 804865) || (targetId == 805215)) {
+				if (env.getDialog() == QuestDialog.START_DIALOG) {
+					return sendQuestDialog(env, 10002);
 				}
-				case 805213:
-				case 805214:
-				case 805215: {
-					switch (env.getDialog()) {
-						case START_DIALOG: {
-							return sendQuestDialog(env, 10002);
-						} case SELECT_REWARD: {
-							qs.setQuestVar(1);
-							qs.setStatus(QuestStatus.REWARD);
-							updateQuestStatus(env);
-							return sendQuestEndDialog(env);
-						} default:
-							return sendQuestEndDialog(env);
-					}
+				else if (env.getDialog() == QuestDialog.SELECT_REWARD) {
+					changeQuestStep(env, 0, 0, true);
+					return sendQuestDialog(env, 5);
 				}
 			}
-		} else if (qs.getStatus() == QuestStatus.REWARD) {
-			if (targetId == 805213 ||
-			    targetId == 805214 ||
-				targetId == 805215) {
-				switch (env.getDialog()) {
-					case SELECT_REWARD: {
-						return sendQuestDialog(env, 5);
-					} default:
-						return sendQuestEndDialog(env);
-				}
+		}
+		else if (qs.getStatus() == QuestStatus.REWARD) {
+			if ((targetId == 804865) || (targetId == 805215)) {
+				return sendQuestEndDialog(env);
 			}
 		}
 		return false;
