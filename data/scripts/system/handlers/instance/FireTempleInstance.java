87516b96306e1adc2c81065bdfb2b631c427a076
MATTYOneInc <mattyonebeatz@gmail.com>
2023-10-08 00:38:05 +1000

    Fix Fire Temple (Like Retail)

--- data/scripts/system/handlers/instance/FireTempleInstance.java
+++ data/scripts/system/handlers/instance/FireTempleInstance.java
@@ -37,9 +37,11 @@ import com.aionemu.gameserver.world.knownlist.Visitor;
 
 import java.util.Set;
 
-/****/
-/** Author (Encom)
-/****/
+/**
+* Author (Encom)
+* Rework: MATTY (ADev.Team)
+* Retail: https://youtu.be/kCY-3m2Mukw
+**/
 
 @InstanceID(320100000)
 public class FireTempleInstance extends GeneralInstanceHandler
@@ -117,6 +119,12 @@ public class FireTempleInstance extends GeneralInstanceHandler
 		int index = dropItems.size() + 1;
         switch (npcId) {
             case 212846: //Kromede The Corrupt.
+                for (Player player: instance.getPlayersInside()) {
+                    if (player.isOnline()) {
+						dropItems.add(DropRegistrationService.getInstance().regDropItem(index++, player.getObjectId(), npcId, 188053994, 1)); //Kromede's Key Bundle.
+                    }
+                }
+				break;
 			case 214621: //Vile Judge Kromede.
                 for (Player player: instance.getPlayersInside()) {
                     if (player.isOnline()) {
@@ -124,81 +132,92 @@ public class FireTempleInstance extends GeneralInstanceHandler
 						dropItems.add(DropRegistrationService.getInstance().regDropItem(index++, player.getObjectId(), npcId, 188053994, 1)); //Kromede's Key Bundle.
                     }
                 }
-            break;
+				break;
 			case 833523: //Kromede's Ornate Treasure Chest.
+                for (Player player: instance.getPlayersInside()) {
+                    if (player.isOnline()) {
+                        dropItems.add(DropRegistrationService.getInstance().regDropItem(index++, player.getObjectId(), npcId, 170030000, 1)); //[Souvenir] Kromede's Mirror.
+                    }
+                }
+				break;
 			case 833524: //Kromede's Brilliant Treasure Chest.
+                for (Player player: instance.getPlayersInside()) {
+                    if (player.isOnline()) {
+                        dropItems.add(DropRegistrationService.getInstance().regDropItem(index++, player.getObjectId(), npcId, 170030000, 1)); //[Souvenir] Kromede's Mirror.
+						dropItems.add(DropRegistrationService.getInstance().regDropItem(index++, player.getObjectId(), npcId, 188052826, 1)); //Judge's Fabled Weapon Chest
+                    }
+                }
+				break;
 			case 833525: //Kromede's Dazzling Treasure Chest.
                 for (Player player: instance.getPlayersInside()) {
                     if (player.isOnline()) {
                         dropItems.add(DropRegistrationService.getInstance().regDropItem(index++, player.getObjectId(), npcId, 170030000, 1)); //[Souvenir] Kromede's Mirror.
-						dropItems.add(DropRegistrationService.getInstance().regDropItem(index++, player.getObjectId(), npcId, 188053083, 1)); //Tempering Solution Chest.
-						dropItems.add(DropRegistrationService.getInstance().regDropItem(index++, player.getObjectId(), npcId, 188053295, 1)); //Empyrean Plume Chest.
+						dropItems.add(DropRegistrationService.getInstance().regDropItem(index++, player.getObjectId(), npcId, 188052826, 1)); //Judge's Fabled Weapon Chest
                     }
                 }
-            break;
+				break;
 			case 212840: //Lava Gatneri.
                 for (Player player: instance.getPlayersInside()) {
                     if (player.isOnline()) {
                         dropItems.add(DropRegistrationService.getInstance().regDropItem(index++, player.getObjectId(), npcId, 188051411, 1)); //Gatneri's Fabled Weapon Chest.
                     }
                 }
-            break;
+				break;
 			case 212842: //Black Smoke Asparn.
                 for (Player player: instance.getPlayersInside()) {
                     if (player.isOnline()) {
                         dropItems.add(DropRegistrationService.getInstance().regDropItem(index++, player.getObjectId(), npcId, 188051412, 1)); //Asparn's Fabled Weapon Chest.
                     }
                 }
-            break;
-			case 212844: //Silver Blade Rotan.
-                switch (Rnd.get(1, 3)) {
-				    case 1:
-				        dropItems.add(DropRegistrationService.getInstance().regDropItem(1, 0, npcId, 190000016, 1)); //Sawteeth Rotan Egg.
-					break;
-					case 2:
-				        dropItems.add(DropRegistrationService.getInstance().regDropItem(1, 0, npcId, 190000037, 1)); //Sawteeth Rotan Egg.
-					break;
-					case 3:
-				        dropItems.add(DropRegistrationService.getInstance().regDropItem(1, 0, npcId, 190000038, 1)); //Sawteeth Rotan Egg.
-					break;
-				}
-            break;
+				break;
         }
     }
 	
 	@Override
     public void onInstanceCreate(WorldMapInstance instance) {
         super.onInstanceCreate(instance);
-		spawn(212846, 421.96918f, 93.555084f, 117.30522f, (byte) 51); //Kromede The Corrupt.
-        switch (Rnd.get(1, 9)) {
-		    case 1:
-				spawn(212839, 153.20758f, 161.87619f, 101.83779f, (byte) 32); //Blue Crystal Molgat.
-			break;
-			case 2:
-				spawn(212840, 151.76723f, 302.36328f, 122.874084f, (byte) 24); //Lava Gatneri.
-			break;
-			case 3:
-				spawn(212784, 151.76723f, 302.36328f, 122.874084f, (byte) 24); //Inferno Spirit.
-			break;
-			case 4:
-				spawn(212841, 350.2237f, 354.806f, 146.613f, (byte) 65); //Flame Branch Flavi.
-			break;
-			case 5:
-				spawn(212842, 304.33118f, 419.86487f, 133.9806f, (byte) 17); //Black Smoke Asparn.
-			break;
-			case 6:
-				spawn(212843, 297.6783f, 202.19652f, 119.36518f, (byte) 61); //Tough Sipus.
-			break;
-			case 7:
-				spawn(212845, 293.1684f, 97.34582f, 128.40712f, (byte) 91); //Broken Wing Kutisen.
-			break;
-			case 8:
-				deleteNpc(212846); //Kromede The Corrupt.
-				spawn(214621, 421.96918f, 93.555084f, 117.30522f, (byte) 51); //Vile Judge Kromede.
-			break;
-			case 9:
-				spawn(798109, 144.581f, 162.128f, 100.931f, (byte) 23); //Liurerk.
-			break;
+		
+        // Random spawns of bosses
+        if (Rnd.get(1, 100) > 25) { // Blue Crystal Molgat
+            spawn(212839, 127.1218f, 176.1912f, 99.67548f, (byte) 15);
+        } else { // elite mob spawns
+            spawn(212790, 127.1218f, 176.1912f, 99.67548f, (byte) 15);
+        }
+
+        if (Rnd.get(1, 100) > 25) { // Black Smoke Asparn
+            spawn(212842, 322.3193f, 431.2696f, 134.5296f, (byte) 80);
+        } else { // elite mob spawns
+            spawn(212799, 322.3193f, 431.2696f, 134.5296f, (byte) 80);
+        }
+
+        if (Rnd.get(1, 100) > 25) { // Lava Gatneri
+            spawn(212840, 153.0038f, 299.7786f, 123.0186f, (byte) 30);
+        } else { // elite mob spawns
+            spawn(212794, 153.0038f, 299.7786f, 123.0186f, (byte) 30);
+        }
+
+        if (Rnd.get(1, 100) > 25) { // Tough Sipus
+            spawn(212843, 296.6911f, 201.9092f, 119.3652f, (byte) 30);
+        } else { // elite mob spawns
+            spawn(212803, 296.6911f, 201.9092f, 119.3652f, (byte) 15);
+        }
+
+        if (Rnd.get(1, 100) > 25) { // Flame Branch Flavi
+            spawn(212841, 350.9276f, 351.7389f, 146.8498f, (byte) 45);
+        } else { // elite mob spawns
+            spawn(212799, 350.9276f, 351.7389f, 146.8498f, (byte) 45);
+        }
+
+        if (Rnd.get(1, 100) > 25) { // Broken Wing Kutisen
+            spawn(212845, 298.7095f, 89.42245f, 128.7143f, (byte) 15);
+        } else { // elite mob spawns
+            spawn(214094, 298.7095f, 89.42245f, 128.7143f, (byte) 15);
+        }
+
+        if (Rnd.get(1, 100) > 90) {// stronger kromede
+            spawn(214621, 421.9935f, 93.18915f, 117.3053f, (byte) 46);
+        } else { // normal kromede
+            spawn(212846, 421.9935f, 93.18915f, 117.3053f, (byte) 46);
         }
     }
 	
