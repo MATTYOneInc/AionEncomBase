a7de0d59d10e5b124b3d1195639870cab22200b4
pressure68 <pressure68@users.noreply.github.com>
2023-10-24 11:56:05 -0400

    quest - 2990 - Making the Daevanion Weapon
    fix by WIZARDMASTER

--- data/scripts/system/handlers/quest/daevanion/_2990Making_The_Daevanion_Weapon.java
+++ data/scripts/system/handlers/quest/daevanion/_2990Making_The_Daevanion_Weapon.java
@@ -12,16 +12,12 @@
  */
 package quest.daevanion;
 
-import com.aionemu.gameserver.model.gameobjects.Npc;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
-import com.aionemu.gameserver.network.aion.serverpackets.SM_DIALOG_WINDOW;
 import com.aionemu.gameserver.questEngine.handlers.QuestHandler;
 import com.aionemu.gameserver.questEngine.model.QuestDialog;
 import com.aionemu.gameserver.questEngine.model.QuestEnv;
 import com.aionemu.gameserver.questEngine.model.QuestState;
 import com.aionemu.gameserver.questEngine.model.QuestStatus;
-import com.aionemu.gameserver.services.QuestService;
-import com.aionemu.gameserver.utils.PacketSendUtility;
 
 /****/
 /** Author Ghostfur & Unknown (Aion-Unique)
@@ -30,7 +26,11 @@ import com.aionemu.gameserver.utils.PacketSendUtility;
 public class _2990Making_The_Daevanion_Weapon extends QuestHandler
 {
 	private final static int questId = 2990;
-	private final static int[] npc_ids = {204146};
+	
+	private int A = 0;
+	private int B = 0;
+	private int C = 0;
+	private int ALL = 0;
 	
 	public _2990Making_The_Daevanion_Weapon() {
 		super(questId);
@@ -38,162 +38,162 @@ public class _2990Making_The_Daevanion_Weapon extends QuestHandler
 	
 	@Override
 	public boolean onLvlUpEvent(QuestEnv env) {
-		return defaultOnLvlUpEvent(env, 2989, true);
+		return defaultOnLvlUpEvent(env, 1989, true);
 	}
 	
 	@Override
 	public void register() {
 		qe.registerOnLevelUp(questId);
+		//int[] mobs = {253696, 253720, 253721, 253682, 253684, 253684};
+		int[] mobs = {213011, 213012, 213009, 213010, 213007, 213008};
+		
 		qe.registerQuestNpc(204146).addOnQuestStart(questId);
-		qe.registerQuestNpc(253696).addOnKillEvent(questId);
-		qe.registerQuestNpc(253720).addOnKillEvent(questId);
-		qe.registerQuestNpc(253721).addOnKillEvent(questId);
-		qe.registerQuestNpc(253682).addOnKillEvent(questId);
-		qe.registerQuestNpc(253684).addOnKillEvent(questId);
-		qe.registerQuestNpc(253685).addOnKillEvent(questId);
-		for (int npc_id: npc_ids) {
-		    qe.registerQuestNpc(npc_id).addOnTalkEvent(questId);
+		qe.registerQuestNpc(204146).addOnTalkEvent(questId);
+		for (int mob: mobs) {
+			qe.registerQuestNpc(mob).addOnKillEvent(questId);
 		}
 	}
 	
 	@Override
 	public boolean onDialogEvent(QuestEnv env) {
-		final Player player = env.getPlayer();
-		int targetId = 0;
-		if (env.getVisibleObject() instanceof Npc)
-			targetId = ((Npc) env.getVisibleObject()).getNpcId();
+		Player player = env.getPlayer();
 		QuestState qs = player.getQuestStateList().getQuestState(questId);
+		QuestDialog dialog = env.getDialog();
+		int targetId = env.getTargetId();
 		if (qs == null || qs.getStatus() == QuestStatus.NONE) {
 			if (targetId == 204146) {
 				if (env.getDialog() == QuestDialog.START_DIALOG) {
-					int plate = player.getEquipment().itemSetPartsEquipped(9);
-					int chain = player.getEquipment().itemSetPartsEquipped(8);
-					int leather = player.getEquipment().itemSetPartsEquipped(7);
-					int cloth = player.getEquipment().itemSetPartsEquipped(6);
-					int gunslinger = player.getEquipment().itemSetPartsEquipped(378);
-					if (plate != 5 && chain != 5 && leather != 5 && cloth != 5 && gunslinger != 5)
-						return sendQuestDialog(env, 4848);
-					else
+					if (isDaevanionArmorEquipped(player)) {
 						return sendQuestDialog(env, 4762);
-				} else
+					} else {
+						return sendQuestDialog(env, 4848);
+					}
+				} else {
 					return sendQuestStartDialog(env);
-			}
-		}
-		if (qs == null)
-			return false;
-		int var = qs.getQuestVarById(0);
-		int var1 = qs.getQuestVarById(1);
-		if (qs.getStatus() == QuestStatus.START) {
-			if (targetId == 204146) {
-				switch (env.getDialog()) {
-					case START_DIALOG:
-						if (var == 0)
-							return sendQuestDialog(env, 1011);
-						if (var == 1)
-							return sendQuestDialog(env, 1352);
-						if (var == 2 && var1 == 60)
-							return sendQuestDialog(env, 1693);
-						if (var == 3 && player.getInventory().getItemCountByItemId(186000040) > 0)
-							return sendQuestDialog(env, 2034);
-					case CHECK_COLLECTED_ITEMS:
-						if (var == 0) {
-							if (QuestService.collectItemCheck(env, true)) {
-								qs.setQuestVarById(0, var + 1);
-								updateQuestStatus(env);
-								return sendQuestDialog(env, 10000);
-							} else
-								return sendQuestDialog(env, 10001);
-						}
-						break;
-					case SELECT_ACTION_1352:
-						if (var == 0)
-							return sendQuestDialog(env, 1352);
-					case SELECT_ACTION_2035:
-					if (var == 3) {
-						if (player.getCommonData().getDp() == 4000 && player.getInventory().getItemCountByItemId(186000040) > 0) {
-							removeQuestItem(env, 186000040, 1);
-							player.getCommonData().setDp(0);
-							qs.setStatus(QuestStatus.REWARD);
-							updateQuestStatus(env);
-							return sendQuestDialog(env, 5);
-						} else
-							return sendQuestDialog(env, 2120);
-					}
-					break;
-					case STEP_TO_2:
-					if (var == 1) {
-						qs.setQuestVarById(0, var + 1);
-						updateQuestStatus(env);
-						PacketSendUtility.sendPacket(player, new SM_DIALOG_WINDOW(env.getVisibleObject().getObjectId(), 10));
-						return true;
-					}
-					break;
-					case STEP_TO_3:
-					if (var == 2) {
-						qs.setQuestVar(3);
-						updateQuestStatus(env);
-						PacketSendUtility.sendPacket(player, new SM_DIALOG_WINDOW(env.getVisibleObject().getObjectId(), 10));
-						return true;
-					}
-					break;
 				}
 			}
-			return false;
+		} else if (qs.getStatus() == QuestStatus.START) {
+			int var = qs.getQuestVarById(0);
+			int var1 = qs.getQuestVarById(1);
+			if (targetId == 204146) {
+				switch (dialog) {
+					case START_DIALOG: {
+						if (var == 0) {
+							return sendQuestDialog(env, 1011);
+						} else if (var == 2 && var1 == 60) {
+							return sendQuestDialog(env, 1693);
+						} else if (var == 3) {
+							return sendQuestDialog(env, 2034);
+						}
+					} case CHECK_COLLECTED_ITEMS: {
+						return checkQuestItems(env, 0, 1, false, 10000, 10001);
+					} case SELECT_ACTION_2035: {
+						int currentDp = player.getCommonData().getDp();
+						int maxDp = player.getGameStats().getMaxDp().getCurrent();
+						long burner = player.getInventory().getItemCountByItemId(186000040);
+						if (currentDp == maxDp && burner >= 1) {
+							removeQuestItem(env, 186000040, 1);
+							player.getCommonData().setDp(0);
+							changeQuestStep(env, 3, 3, true);
+							return sendQuestDialog(env, 5);
+						} else {
+							return sendQuestDialog(env, 2120);
+						}
+					} case STEP_TO_2: {
+						return defaultCloseDialog(env, 1, 2);
+					} case STEP_TO_3: {
+						qs.setQuestVar(3);
+						updateQuestStatus(env);
+						return sendQuestSelectionDialog(env);
+					} case FINISH_DIALOG: {
+						return sendQuestSelectionDialog(env);
+					}
+				}
+			}
 		} else if (qs.getStatus() == QuestStatus.REWARD) {
 			if (targetId == 204146) {
 				return sendQuestEndDialog(env);
 			}
-			return false;
 		}
 		return false;
-	  }
+	}
 	
 	@Override
-    public boolean onKillEvent(QuestEnv env) {
+	public boolean onKillEvent(QuestEnv env) {
 		Player player = env.getPlayer();
 		QuestState qs = player.getQuestStateList().getQuestState(questId);
-		if (qs == null || qs.getStatus() != QuestStatus.START) {
-			return false;
-		}
-		int var1 = qs.getQuestVarById(1);
-		int var2 = qs.getQuestVarById(2);
-		int var3 = qs.getQuestVarById(3);
-		int targetId = env.getTargetId();
-        if ((targetId == 253696 || //Smoking Ember.
-	        targetId == 253720 || //Lava Hoverstone.
-		    targetId == 253721 || //Lava Hoverstone.
-		    targetId == 253682 || //Sulfur Oculazen.
-		    targetId == 253684 || //Sulfur Oculazen.
-		    targetId == 253685) && qs.getQuestVarById(0) == 2) {
-			switch (targetId) {
-				case 253696: //Smoking Ember.
-				    if (var1 >= 0 && var1 < 30) {
-					    ++var1;
-					    qs.setQuestVarById(1, var1 + 1);
-					    updateQuestStatus(env);
-				    }
-				break;
-                case 253720: //Lava Hoverstone.
-			    case 253721: //Lava Hoverstone.
-                    if (var2 >= 0 && var2 < 30) {
-					    ++var2;
-					    qs.setQuestVarById(2, var2 + 3);
-					    updateQuestStatus(env);
-                    }
-                break;
-                case 253682: //Sulfur Oculazen.
-			    case 253684: //Sulfur Oculazen.
-			    case 253685: //Sulfur Oculazen.
-                    if (var3 >= 0 && var3 < 30) {
-					    ++var3;
-					    qs.setQuestVarById(3, var3 + 7);
-					    updateQuestStatus(env);
-                    }
-				break;
+		if (qs != null && qs.getStatus() == QuestStatus.START) {
+			int var = qs.getQuestVarById(0);
+			if (var == 2) {
+				switch (env.getTargetId()) {
+					case 213011: // Meek Crestlich
+					case 213012: // Meek Crestlich
+					{ 
+						if (A >= 0 && A < 30) {
+							++A;
+							ALL = C;
+							ALL = ALL << 7;
+							ALL += B;
+							ALL = ALL << 7;
+							ALL += A;
+							ALL = ALL << 7;
+							ALL += 2;
+							qs.setQuestVar(ALL);
+							updateQuestStatus(env);
+						}
+						break;
+					}
+					case 213009: // Blue Jaw Monitor
+					case 213010: // Blue Jaw Monitor
+					{
+						if (B >= 0 && B < 30) {
+							++B;
+							ALL = C;
+							ALL = ALL << 7;
+							ALL += B;
+							ALL = ALL << 7;
+							ALL += A;
+							ALL = ALL << 7;
+							ALL += 2;
+							qs.setQuestVar(ALL);
+							updateQuestStatus(env);
+						}
+						break;
+					}
+					case 213007: // Plains Vespine
+					case 213008: // Plains Vespine
+					{ 
+						if (C >= 0 && C < 30) {
+							++C;
+							ALL = C;
+							ALL = ALL << 7;
+							ALL += B;
+							ALL = ALL << 7;
+							ALL += A;
+							ALL = ALL << 7;
+							ALL += 2;
+							qs.setQuestVar(ALL);
+							updateQuestStatus(env);
+						}
+						break;
+					}
+				} if (qs.getQuestVarById(0) == 2 && A == 30 && B == 30 && C == 30) {
+					qs.setQuestVarById(1, 60);
+					updateQuestStatus(env);
+					return true;
+				}
 			}
-		} if (qs.getQuestVarById(0) == 2 && var1 == 30 && var2 == 30 && var3 == 30) {
-			qs.setQuestVarById(1, 60);
-			updateQuestStatus(env);
+		}
+		return false;
+	}
+	
+	private boolean isDaevanionArmorEquipped(Player player) {
+		int plate = player.getEquipment().itemSetPartsEquipped(9);
+		int chain = player.getEquipment().itemSetPartsEquipped(8);
+		int leather = player.getEquipment().itemSetPartsEquipped(7);
+		int cloth = player.getEquipment().itemSetPartsEquipped(6);
+		int gunslinger = player.getEquipment().itemSetPartsEquipped(378);
+		if (plate == 5 || chain == 5 || leather == 5 || cloth == 5 || gunslinger == 5) {
 			return true;
 		}
 		return false;
