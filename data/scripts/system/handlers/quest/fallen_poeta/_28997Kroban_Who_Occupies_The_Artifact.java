15be4dec48ca6557986b455ad9147e49ab1b58f2
pressure68 <pressure68@users.noreply.github.com>
2023-11-10 10:37:21 -0500

    quest fixes - DainAvenger
    fixed kill counters, fixed some npc_type

--- data/scripts/system/handlers/quest/fallen_poeta/_28997Kroban_Who_Occupies_The_Artifact.java
+++ data/scripts/system/handlers/quest/fallen_poeta/_28997Kroban_Who_Occupies_The_Artifact.java
@@ -90,6 +90,7 @@ public class _28997Kroban_Who_Occupies_The_Artifact extends QuestHandler
 					qs.setQuestVarById(1, qs.getQuestVarById(1) + 1);
 					updateQuestStatus(env);
 				} if (qs.getQuestVarById(1) >= 1) {
+					qs.setQuestVarById(0, 1);
 					qs.setStatus(QuestStatus.REWARD);
 					updateQuestStatus(env);
 				}
