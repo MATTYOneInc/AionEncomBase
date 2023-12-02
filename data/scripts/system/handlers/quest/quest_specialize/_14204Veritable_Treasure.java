1863ac59658ad02778462a5bbddf7e043761ea99
Dezalmado <51215558+dezalmado@users.noreply.github.com>
2023-12-02 19:53:13 -0300

    fixed by #DainAvenger

--- data/scripts/system/handlers/quest/quest_specialize/_14204Veritable_Treasure.java
+++ data/scripts/system/handlers/quest/quest_specialize/_14204Veritable_Treasure.java
@@ -86,6 +86,7 @@ public class _14204Veritable_Treasure extends QuestHandler
 					qs.setQuestVarById(1, qs.getQuestVarById(1) + 1);
 					updateQuestStatus(env);
 				} if (qs.getQuestVarById(1) >= 8) {
+					qs.setQuestVarById(0, 1);
 					qs.setStatus(QuestStatus.REWARD);
 					updateQuestStatus(env);
 				}
