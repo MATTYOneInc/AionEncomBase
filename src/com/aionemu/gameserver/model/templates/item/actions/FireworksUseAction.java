d928321b99a506f42c270e3ccbb8dd838264dda7
Yayaya <35761098+podpol@users.noreply.github.com>
2024-03-21 10:47:22 +0200

    FIX fireworks not visible to other players.
    Some action must be visible for others, same as fireworks action!?
    so we need to show action to other players not only to yourself)

--- src/com/aionemu/gameserver/model/templates/item/actions/FireworksUseAction.java
+++ src/com/aionemu/gameserver/model/templates/item/actions/FireworksUseAction.java
@@ -20,13 +20,19 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlType;
 
+import com.aionemu.gameserver.controllers.observer.ItemUseObserver;
+import com.aionemu.gameserver.model.DescriptionId;
+import com.aionemu.gameserver.model.TaskId;
 import com.aionemu.gameserver.model.gameobjects.Item;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_ITEM_USAGE_ANIMATION;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_SYSTEM_MESSAGE;
 import com.aionemu.gameserver.utils.PacketSendUtility;
+import com.aionemu.gameserver.utils.ThreadPoolManager;
 
 /**
  * @author Tiger
+ * mod yayaya
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "FireworksUseAction")
@@ -38,13 +44,40 @@ public class FireworksUseAction extends AbstractItemAction {
 	}
 
 	@Override
-	public void act(Player player, Item parentItem, Item targetItem) {
-		if (parentItem.getActivationCount() > 1) {
-			parentItem.setActivationCount(parentItem.getActivationCount() - 1);
-		} else {
-			player.getInventory().decreaseByObjectId(parentItem.getObjectId(), 1);
-		}
-		PacketSendUtility.sendPacket(player, new SM_ITEM_USAGE_ANIMATION(player.getObjectId(), parentItem.getObjectId(),
-				parentItem.getItemTemplate().getTemplateId(), 0, 1, 0));
+	public void act(final Player player, final Item parentItem, Item targetItem) {
+
+		player.getController().cancelUseItem();
+		PacketSendUtility.broadcastPacketAndReceive(player, new SM_ITEM_USAGE_ANIMATION(player.getObjectId(),
+				parentItem.getObjectId(), parentItem.getItemId(), 0, 1, 0));
+
+		final ItemUseObserver observer = new ItemUseObserver() {
+
+			@Override
+			public void abort() {
+				// TODO: abort is invalid. Should we abort all or only the last syncid?
+				player.getController().cancelTask(TaskId.ITEM_USE);
+				player.removeItemCoolDown(parentItem.getItemTemplate().getUseLimits().getDelayId());
+				PacketSendUtility.sendPacket(player, SM_SYSTEM_MESSAGE
+						.STR_ITEM_CANCELED(new DescriptionId(parentItem.getItemTemplate().getNameId())));
+				PacketSendUtility.broadcastPacket(player, new SM_ITEM_USAGE_ANIMATION(player.getObjectId(),
+						parentItem.getObjectId(), parentItem.getItemTemplate().getTemplateId(), 0, 1, 0), true);
+				player.getObserveController().removeObserver(this);
+			}
+		};
+		player.getObserveController().attach(observer);
+		player.getController().addTask(TaskId.ITEM_USE, ThreadPoolManager.getInstance().schedule(new Runnable() {
+			@Override
+			public void run() {
+				player.getObserveController().removeObserver(observer);
+				if (parentItem.getActivationCount() > 1) {
+					parentItem.setActivationCount(parentItem.getActivationCount() - 1);
+				} else {
+					player.getInventory().decreaseByObjectId(parentItem.getObjectId(), 1);
+				}
+
+				PacketSendUtility.broadcastPacketAndReceive(player, new SM_ITEM_USAGE_ANIMATION(player.getObjectId(),
+						parentItem.getObjectId(), parentItem.getItemId(), 0, 1, 0));
+			}
+		}, 100));
 	}
 }
