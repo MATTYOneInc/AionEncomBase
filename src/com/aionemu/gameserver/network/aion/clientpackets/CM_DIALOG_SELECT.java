82c0f0cbadb17df7f63998bb51b8fe6b859e1c8a
Dezalmado <jailton1991@outlook.com>
2024-07-06 10:24:30 -0300

    fixes by DainAvenger
    When you talk to an NPC, you will no longer be dismounted automatically. "need to check"

--- src/com/aionemu/gameserver/network/aion/clientpackets/CM_DIALOG_SELECT.java
+++ src/com/aionemu/gameserver/network/aion/clientpackets/CM_DIALOG_SELECT.java
@@ -32,12 +32,6 @@ import com.aionemu.gameserver.questEngine.model.QuestEnv;
 import com.aionemu.gameserver.services.ClassChangeService;
 import com.aionemu.gameserver.services.QuestService;
 
-/**
- * @author KKnD
- * @author orz
- * @author avol
- * @author BeckUp.Media
- */
 public class CM_DIALOG_SELECT extends AionClientPacket {
 	private int targetObjectId;
 	private int dialogId;
@@ -46,23 +40,14 @@ public class CM_DIALOG_SELECT extends AionClientPacket {
 	private int lastPage;
 	private int questId;
 	private int unk;
-	private int unk02;
 
 	@SuppressWarnings("unused")
 	private static final Logger log = LoggerFactory.getLogger(CM_DIALOG_SELECT.class);
 
-	/**
-	 * Constructs new instance of <tt>CM_CM_REQUEST_DIALOG </tt> packet
-	 *
-	 * @param opcode
-	 */
 	public CM_DIALOG_SELECT(int opcode, State state, State... restStates) {
 		super(opcode, state, restStates);
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
 	@Override
 	protected void readImpl() {
 		targetObjectId = readD();
@@ -71,17 +56,18 @@ public class CM_DIALOG_SELECT extends AionClientPacket {
 		unk = readH();
 		lastPage = readH();
 		questId = readD();
-		unk02 = readH(); // todo need to be checked for what - BeckUp.Media
+		readH();
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
 	@Override
 	protected void runImpl() {
 		final Player player = getConnection().getActivePlayer();
 		QuestTemplate questTemplate = DataManager.QUEST_DATA.getQuestById(questId);
 		QuestEnv env = new QuestEnv(null, player, questId, 0);
+
+        /* 	if (player.isInPlayerMode(PlayerMode.RIDE)) { - dismount player with pet when interact with npc.
+			 player.unsetPlayerMode(PlayerMode.RIDE);
+        } */
 
 		if (player.isTrading()) {
 			return;
